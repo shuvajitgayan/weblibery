4432c37f998279697114f9c8ed0a91c836c2a21a
Shuvajit Gayen <127286866+shuvajitgayan@users.noreply.github.com>
2024-03-14 07:46:14 +0000

    logobar line mistack fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      
+      <body className={inter.className}>
       <LogoBar />
-      <body className={inter.className}>{children}</body>
+        {children}
+        </body>
     </html>
   );
 }
