74e4216f222ea68770790d9afd99287b09e269be
Shuvajit Gayen <127286866+shuvajitgayan@users.noreply.github.com>
2024-03-20 06:03:28 +0000

    added read more button

--- src/components/card/cars.tsx
+++ src/components/card/cars.tsx
@@ -5,6 +5,7 @@ import { IconAppWindow } from "@tabler/icons-react";
 import Image from "next/image";
 import Books_data from "../../data/books_data.json"
 import Link from "next/link";
+import ghost from '../../../public/ghost.jpg'
 
 interface Books{
   
@@ -29,24 +30,33 @@ export default function Books() {
         <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-3xl">Learn with our team</p>
       </div>
       <div className="mt-10">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center text-slate-50">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center text-slate-50">
           {Regular_Uses.map((books:Books)=>(
             <div className="flex justify-center" key={books.id}>
               <BackgroundGradient className="flex flex-col overflow-hidden rounded-[22px] bg-white dark:bg-zinc-900 h-full w-auto text-slate-700">
-                <div className="p-2 sm:p-6 flex flex-col items-center text-center flex-grow">
-                  
-                  <p>{books.title}</p>
-                  <p>{books.instructor}</p>
-                  <p>{books.description}</p>
-                  <Link href={'https://www.amazon.in/Books/b?ie=UTF8&node=976389031&ref_=nav_custrec_signin&'}> Buy Now ${books.price} </Link>
+                <div className="p-2 sm:p-6 flex flex-col items-center text-center flex-grow ">
+                  <Image className=""
+                  height={100}
+                  width={100}
+                  src={ghost}
+                  alt="book image"/>
+
+                  <p className="mt-2 text-xl">{books.title}</p>
+                  <p className="mt-2">{books.instructor}</p>
+                  <p className="mt-2">{books.description}</p>
+                  <Link 
+                  className="p-1 bg-stone-400 text-blue-500 hover:bg-slate-100 rounded mt-3"
+                  href={books.website_link}>
+                     Read more
+                  </Link>
                 </div>
               </BackgroundGradient>
             </div>
           ))}
         </div>
       </div>
-      <div className="mt-20 text-center">
-        <Link className="p-2 bg-slate-400 text-white text-xs md:text-2xl sm:text-xl lg:text-4xl rounded"
+      <div className="mt-10 text-center">
+        <Link className="p-2 bg-slate-400 text-white text-xs rounded"
         href={'https://www.overdrive.com/apps/libby'} >
           View All
         </Link>
